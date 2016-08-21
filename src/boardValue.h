a76299e7c2f435bb5f4fe58b2a19d18892774774
David Pfeiffer <djpfeiffer1s@semo.edu>
2016-08-21 12:34:08 -0600

    Revert "Started work to board value class"
    This reverts commit ee19a31e31a15b020acce6e0200489a144858427.

--- src/boardValue.h
+++ src/boardValue.h
@@ -1,56 +1,6 @@
 #ifndef __BOARDVALUE
 #define __BOARDVALUE
 
-#include "playerValue.h"
-
-enum BoardPieceValue {Empty, O, X};
-
-class BoardValue
-{
-  public:
-  
-  BoardValue(BoardPieceValue boardPieceValue)
-  {
-    set(boardPieceValue);
-  }
-  
-  friend bool operator== (const BoardValue & left, const BoardValue & right)
-  {
-    return left.boardPieceValue == right.boardPieceValue && left.playerValue == right.playerValue;
-  }
-  
-  friend bool operator!= (const BoardValue & left, const BoardValue & right)
-  {
-    return left.boardPieceValue != right.boardPieceValue || left.playerValue != right.playerValue;
-  }
-  
-  void set(BoardPieceValue boardPieceValue)
-  {
-    this->boardPieceValue = boardPieceValue;
-    if (boardPieceValue == BoardPieceValue::O)
-    {
-      this->playerValueSet = true;
-      this->playerValue = PlayerValue::PlayerOne;
-    }
-    else if (boardPieceValue == BoardPieceValue::X) this->playerValue = PlayerValue::PlayerTwo;
-  }
-  
-  bool isEmpty()
-  {
-    return this->boardPieceValue == BoardPieceValue::Empty;
-  }
-  
-  bool isPlayerBoardValue(const PlayerValue & playerValue)
-  {
-    return this->playerValue == playerValue;
-  }
-  
-  private:
-  
-  bool boardPieceValueSet = false, playerValueSet = false;
-  
-  BoardPieceValue boardPieceValue;
-  PlayerValue playerValue;
-}
+enum BoardValue {Empty, O, X};
 
 #endif
