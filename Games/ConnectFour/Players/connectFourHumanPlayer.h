606b8951cdfaf94335760cd3abb5042da69655ad
David Pfeiffer <github@davidjpfeiffer.com>
2017-03-28 19:19:59 -0500

    implement human player

--- Games/ConnectFour/Players/connectFourHumanPlayer.h
+++ Games/ConnectFour/Players/connectFourHumanPlayer.h
@@ -22,7 +22,39 @@ class ConnectFourHumanPlayer : public ConnectFourPlayer
   
   void getMove(GameState * p_gameState) const
   {
-    this->gameDefinition.makeRandomMove(p_gameState, this->getPlayerValue());
+    ConnectFourGameState * gameState = (ConnectFourGameState *)p_gameState;
+    ConnectFourGameState gameStateAfterMove;
+    
+    bool validMoveMade = false;
+    unsigned column, indexOfColumn;
+
+    std::cout << this->getName() << "'s turn!\n";
+
+    while (!validMoveMade)
+    {
+      std::cout << "Enter the column: ";
+      std::cin >> column;
+      indexOfColumn = column - 1;
+
+      gameStateAfterMove.board = gameState->board;
+      
+      if (ConnectFourBoard::isValidColumn(indexOfColumn))
+      {
+        gameStateAfterMove.board.set(indexOfColumn, this->getBoardValue());
+        validMoveMade = this->gameDefinition.isValidMove(gameState, & gameStateAfterMove, this->getPlayerValue());
+      }
+      else
+      {
+        std::cout << "Column must be between 1 and 7 inclusive.\n";
+      }
+
+      if (!validMoveMade)
+      {
+        std::cout << "That was not a valid move. Please try again.\n";
+      }
+    }
+    
+    gameState->board = gameStateAfterMove.board;
   }
 };
 
